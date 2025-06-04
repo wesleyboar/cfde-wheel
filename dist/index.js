33969a59f204fabfe4b25a5defa1dd0c568415b0
John Erol Evangelista <je.evangelista04@gmail.com>
2025-06-04 10:57:59 -0400

    updated wheel

--- dist/index.js
+++ dist/index.js
@@ -3770,13 +3770,6 @@ var dccs = [
     description: "Cellular spatial atlas of the human body"
   },
   {
-    id: void 0,
-    short_label: "ComPASS",
-    homepage: "https://commonfund.nih.gov/compass",
-    icon: "/img/interactive/compass.svg",
-    description: "Community-led research on structural drivers of health"
-  },
-  {
     id: "d6bb00c3-7224-5001-b9c5-9838622fba40",
     short_label: "4DN",
     homepage: "https://info.cfde.cloud/dccs/4DN",
@@ -3833,6 +3826,13 @@ var dccs = [
     description: "Metabolomics"
   },
   {
+    id: void 0,
+    short_label: "SCGE",
+    homepage: "https://commonfund.nih.gov/editing",
+    icon: "/img/interactive/scge.png",
+    description: "Reducing the burden of diseases caused by genetic changes"
+  },
+  {
     id: "2399794e-74c6-5735-a039-0782cdeeb1e2",
     short_label: "SPARC",
     homepage: "https://info.cfde.cloud/dccs/SPARC",
@@ -3881,31 +3881,39 @@ var centers = [
     endpoint: "https://info.cfde.cloud/centers/CWIC",
     rotate: "-72deg",
     position: {
-      left: 85,
-      top: 310
+      left: { md: 85, xs: 60 },
+      top: { md: 310, xs: 320 }
     },
     text_position: {
-      top: 80,
-      left: "50%"
+      top: { md: 80, xs: 70 },
+      left: { md: "50%", xs: "65%" }
     },
     image_position: {
       top: 100,
       left: "15%"
+    },
+    image_position_small: {
+      top: 70,
+      left: "25%"
     }
   },
   {
     label: "knowledge",
     endpoint: "https://info.cfde.cloud/centers/KC",
     position: {
-      left: 60,
-      top: 450
+      left: { md: 60, xs: 40 },
+      top: { md: 450, xs: 423 }
     },
     text_position: {
-      top: 120,
+      top: { md: 120, xs: 100 },
       left: "10%"
     },
     image_position: {
       top: 50,
+      left: "17%"
+    },
+    image_position_small: {
+      top: 45,
       left: "17%"
     }
   },
@@ -3914,15 +3922,19 @@ var centers = [
     endpoint: "https://info.cfde.cloud/centers/TC",
     rotate: "72deg",
     position: {
-      left: -83,
-      top: 467
+      left: { md: -83, xs: -65 },
+      top: { md: 467, xs: 434 }
     },
     text_position: {
-      top: 105,
+      top: { md: 105, xs: 90 },
       left: "10%"
     },
     image_position: {
       top: 35,
+      left: "35%"
+    },
+    image_position_small: {
+      top: 33,
       left: "35%"
     }
   },
@@ -3931,16 +3943,20 @@ var centers = [
     endpoint: "https://info.cfde.cloud/centers/DRC",
     rotate: "144deg",
     position: {
-      left: -143,
-      top: 338
+      left: { md: -143, xs: -110 },
+      top: { md: 338, xs: 337 }
     },
     text_position: {
-      top: 50,
-      left: "25%"
+      top: { md: 50, xs: 50 },
+      left: { md: "25%", xs: "10%" }
     },
     image_position: {
       top: 75,
       left: "60%"
+    },
+    image_position_small: {
+      top: 55,
+      left: "45%"
     }
   },
   {
@@ -3948,20 +3964,25 @@ var centers = [
     endpoint: "https://info.cfde.cloud/centers/ICC",
     rotate: "216deg",
     position: {
-      left: -39,
-      top: 240
+      left: { md: -39, xs: -31 },
+      top: { md: 240, xs: 265 }
     },
     text_position: {
-      top: 60,
+      top: { md: 60, xs: 39 },
       left: "23%"
     },
     image_position: {
       top: 110,
       left: "47%"
+    },
+    image_position_small: {
+      top: 65,
+      left: "47%"
     }
   }
 ];
 var radius = 280;
+var radius_small = 195;
 var pie_chunk = 2 * Math.PI / dccs.length;
 function InteractiveNavComponent() {
   return /* @__PURE__ */ (0, import_jsx_runtime2.jsxs)(import_material.Container, { sx: { position: "relative", width: 200 }, children: [
@@ -3969,6 +3990,8 @@ function InteractiveNavComponent() {
       const angle = pie_chunk * i;
       const x = radius * Math.cos(angle);
       const y = radius * Math.sin(angle) + 400;
+      const x_small = radius_small * Math.cos(angle);
+      const y_small = radius_small * Math.sin(angle) + 400;
       return /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Tooltip, { title: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Typography, { children: dcc.description }), children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
         import_material.Button,
         {
@@ -3976,10 +3999,10 @@ function InteractiveNavComponent() {
           sx: {
             background: "#fff",
             borderRadius: 1e3,
-            width: 90,
-            height: 90,
+            width: { md: 90, xs: 60 },
+            height: { md: 90, xs: 60 },
             position: "absolute",
-            transform: `translate(${x}px, ${y}px)`
+            transform: { md: `translate(${x}px, ${y}px)`, xs: `translate(${x_small}px, ${y_small}px)` }
           },
           children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Link, { href: dcc.homepage, target: "_blank", rel: "noopener noreferrer", children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: dcc.icon || "", alt: dcc.short_label || "", width: 70, height: 70 }) })
         }
@@ -3988,16 +4011,17 @@ function InteractiveNavComponent() {
     centers.map((center, i) => {
       return /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Button, { sx: __spreadValues({
         position: "absolute",
-        width: 190,
-        height: 190
+        width: { md: 190, xs: 150 },
+        height: { md: 190, xs: 150 }
       }, center.position || {}), children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Container, { sx: {
         position: "relative",
-        width: 160,
-        height: 190
+        width: { md: 190, xs: 150 },
+        height: { md: 190, xs: 150 }
       }, children: /* @__PURE__ */ (0, import_jsx_runtime2.jsxs)("a", { href: center.endpoint, children: [
         /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: `/img/interactive/${center.label}.png`, alt: center.label, loading: "lazy", decoding: "async", sizes: "100vw", style: { position: "absolute", height: "100%", width: "100%", inset: "0px", objectFit: "contain", color: "transparent", transform: `rotate(${center.rotate || "0deg"})` } }),
-        /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Typography, { sx: __spreadValues({ color: "#FFF", position: "absolute", textTransform: "uppercase" }, center.text_position || {}), children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("b", { children: center.label }) }),
-        /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: `/img/interactive/${center.label} 1.png`, alt: center.label, width: 40, height: 40, style: __spreadValues({ position: "absolute", zIndex: 100 }, center.image_position || {}) })
+        /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Typography, { sx: __spreadValues({ color: "#FFF", position: "absolute", textTransform: "uppercase", fontSize: { md: 16, xs: 11.5 } }, center.text_position || {}), children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("b", { children: center.label }) }),
+        /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Container, { sx: { display: { md: "block", xs: "none" } }, children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: `/img/interactive/${center.label} 1.png`, alt: center.label, width: 40, height: 40, style: __spreadValues({ position: "absolute", zIndex: 100 }, center.image_position || {}) }) }),
+        /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Container, { sx: { display: { md: "none", xs: "block" } }, children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: `/img/interactive/${center.label} 1.png`, alt: center.label, width: 30, height: 30, style: __spreadValues({ position: "absolute", zIndex: 100 }, center.image_position_small || {}) }) })
       ] }) }) }, center.label);
     }),
     /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
@@ -4007,15 +4031,15 @@ function InteractiveNavComponent() {
         sx: {
           background: "#fff",
           borderRadius: 1e3,
-          width: 120,
-          height: 120,
+          width: { md: 120, xs: 110 },
+          height: { md: 120, xs: 110 },
           position: "absolute",
-          top: 395,
-          left: 5,
+          top: { md: 395, xs: 365 },
+          left: { md: 5, xs: -5 },
           padding: 1,
           zIndex: 100
         },
-        children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Link, { href: "/info", children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: "../img/interactive/CFDE_logo.png", alt: "cfde_logo", width: 150, height: 150, style: { zIndex: 100 } }) })
+        children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_material.Link, { href: "https://info.cfde.cloud", children: /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("img", { src: "../img/interactive/CFDE_logo.png", alt: "cfde_logo", width: 150, height: 150, style: { zIndex: 100 } }) })
       }
     ),
     /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
@@ -4024,11 +4048,11 @@ function InteractiveNavComponent() {
         sx: {
           background: "#000",
           borderRadius: "50%",
-          width: 230,
-          height: 230,
+          width: { md: 230, xs: 200 },
+          height: { md: 230, xs: 200 },
           position: "absolute",
-          top: 340,
-          left: -50,
+          top: { md: 340, xs: 320 },
+          left: { md: -50, xs: -50 },
           opacity: "0.5"
         }
       }
