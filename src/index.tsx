6e069c4cd774ff2dd27d32bcc2d5ff4ec931d6d9
John Erol Evangelista <je.evangelista04@gmail.com>
2025-10-15 12:42:54 -0400

    updated position

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,7 @@ import { Container } from "@mui/material";
 const CFDEWheel = () => {
 	return (
 		<InteractiveNavModal>
-			<Container sx={{height: "100vh", width: "100vw"}}>
 				<InteractiveNavComponent/>
-			</Container>
 		</InteractiveNavModal>
 	)
 }
--- src/interactive.tsx
+++ src/interactive.tsx
@@ -1,47 +1,59 @@
 import { Container, Button, IconButton, Typography, Tooltip, Link, Box } from "@mui/material";
 import dccs from './dccs.json'
+import { useEffect, useState } from "react";
 
-  const centers = [
+  const centers = (radius:number) => ([
+	{
+			label: "coordination",
+			endpoint: "https://info.cfde.cloud/centers/ICC",
+			rotate: '216deg',
+			position: {
+				// right: "50%",
+				// top: "0%"
+				// top: 60
+				transform: `translate(${-radius/20}px, ${radius/3.2}px)`
+			},
+			text_position: {
+				top: "30%",
+				left: "23%"
+			},
+			image_position: {
+				top: "60%",
+				left: "47%"
+			}
+		},
 		{
 			label: "cloud",
 			endpoint: "https://info.cfde.cloud/centers/CWIC",
 			rotate: "-72deg",
 			position: {
-				left: {md: 85, xs: 60},
-				top: {md: 310, xs: 320}
+				// left: "40%",
+				// top: "70%"
+				transform: `translate(${radius/2.5}px, ${radius/1.77}px)`
 			},
 			text_position: {
-				top: {md: 80, xs: 55},
-				left: {md: "50%", xs: "63%"},
+				top: "50%",
+				left: "55%"
 
 			},
 			image_position: {
-				top: 90,
+				top: "50%",
 				left: "20%"
-			},
-			image_position_small: {
-				top: 70,
-				left: "30%"
 			}
 		},
 		{
 			label: "knowledge",
 			endpoint: "https://info.cfde.cloud/centers/KC",
 			position: {
-				left: {md: 58, xs: 40},
-				top: {md: 449, xs: 420}
+				transform: `translate(${radius/3.25}px, ${radius*1.07}px)`
 			},
 			text_position: {
-				top: {md: 125, xs: 100},
+				top: "75%",
 				left: "10%"
 			},
 			image_position: {
-				top: 53,
+				top: "30%",
 				left: "13%"
-			},
-			image_position_small: {
-				top: 45,
-				left: "15%"
 			}
 		},
 		{
@@ -49,20 +61,15 @@ import dccs from './dccs.json'
 			endpoint: "https://info.cfde.cloud/centers/TC",
 			rotate: "72deg",
 			position: {
-				left: {md: -83, xs: -65},
-				top: {md: 467, xs: 434}
+				transform: `translate(${-radius/5}px, ${radius*1.135}px)`
 			},
 			text_position: {
-				top: {md: 105, xs: 85},
+				top: "65%",
 				left: "15%"
 			},
 			image_position: {
-				top: 35,
+				top: "20%",
 				left: "45%"
-			},
-			image_position_small: {
-				top: 30,
-				left: "42%"
 			}
 		},
 		{
@@ -70,47 +77,21 @@ import dccs from './dccs.json'
 			endpoint: "https://info.cfde.cloud/centers/DRC",
 			rotate: "144deg",
 			position: {
-				left: {md: -143, xs: -110},
-				top: {md: 338, xs: 337}
+				transform: `translate(${-radius/2.4}px, ${radius/1.49}px)`
 			},
 			text_position: {
-				top: {md: 50, xs: 30},
-				left: {md: "25%", xs: "12%"}
+				top: "25%",
+				left: "25%"
 			},
 			image_position: {
-				top: 65,
-				left: "55%"
-			},
-			image_position_small: {
-				top: 55,
-				left: "45%"
+				top: "35%",
+				left: "60%"
 			}
 		},
-		{
-			label: "coordination",
-			endpoint: "https://info.cfde.cloud/centers/ICC",
-			rotate: '216deg',
-			position: {
-				left: {md: -39, xs: -31},
-				top: {md: 240, xs: 265}
-			},
-			text_position: {
-				top: {md: 60, xs: 30},
-				left: "23%"
-			},
-			image_position: {
-				top: 100,
-				left: "47%"
-			},
-			image_position_small: {
-				top: 55,
-				left: "47%"
-			}
-		},
-	]
+	])
 
-const radius = 280
-const radius_small = 200
+const default_radius = 280
+// const radius_small = 200
 interface dccType {
 	id: string
 	short_label: string
@@ -119,37 +100,59 @@ interface dccType {
 	description?: string 
 }
 
+function useViewportDimensions() {
+  const [dimensions, setDimensions] = useState(() => ({ height: window.innerHeight, width: window.innerWidth }))
+  useEffect(() => {
+    const ctx: any = {}
+    const listener = () => {
+      if (ctx.resizeTimeout) clearTimeout(ctx.resizeTimeout);
+      ctx.resizeTimeout = setTimeout(() => {
+        setDimensions({
+          height: window.innerHeight,
+          width: window.innerWidth,
+        })
+      }, 200);
+    };
+    window.addEventListener('resize', listener)
+    return () => window.removeEventListener('resize', listener)
+  }, [])
+  return dimensions
+}
+
 export default function InteractiveNavComponent() {
 	const pie_chunk = 2*Math.PI/(dccs.length)
+	const dimensions = useViewportDimensions()
+	const radius = Math.max(Math.min(dimensions.width/2, dimensions.height/2, default_radius) - 30, 100)
 	return (
-		<Container sx={{position: "relative", width: "fit-content", height: "fit-content"}}>
+		// <Container>
+			<Container sx={{position: "relative", display: "flex", justifyContent: "center"}}>
 				{dccs.map((dcc:any, i:number)=>{
 					const angle = pie_chunk * i
 					const x = radius * Math.cos(angle)
-					const y = radius * Math.sin(angle) + 400
-					const x_small = radius_small * Math.cos(angle)
-					const y_small = radius_small * Math.sin(angle) + 400
-					
+					const y = radius * Math.sin(angle) + radius
+					// const x_small = radius_small * Math.cos(angle)
+					// const y_small = radius_small * Math.sin(angle) + radius_small
 					return (
-						<Tooltip title={<Typography>{dcc.description}</Typography>} key={dcc.short_label || ''} >
+						<Tooltip title={<Typography>{dcc.description}</Typography>} key={dcc.label || ''} >
 							<IconButton 
 								// disableElevation
 								sx={{
 									background: "#fff", 
 									borderRadius: 1000, 
-									width: {md: 90, xs: 60}, 
-									height:  {md: 90, xs: 60},
+									width: radius/3.5, 
+									height:  radius/3.5,
 									position: "absolute",
-									transform: {md: `translate(${x}px, ${y}px)`, xs: `translate(${x_small}px, ${y_small}px)`},
+									// transform: {md: `translate(${x}px, ${y}px)`, xs: `translate(${x_small}px, ${y_small}px)`},
+									transform: `translate(${x}px, ${y}px)`
 								}}
 							>
 								<Link href={dcc.homepage} target="_blank" rel="noopener noreferrer">
 									<Box 
 										sx={{
 											position: 'relative',
-											width: {md: 60, xs: 40},
+											width: radius/5,
 											overflow: 'hidden',
-											height: {md: 60, xs: 40}
+											height: radius/5
 										}}
 									>
 										<img src={dcc.icon || ''} alt={dcc.short_label || ''} style={{
@@ -167,22 +170,22 @@ export default function InteractiveNavComponent() {
 						</Tooltip>
 					)
 				})}
-				{centers.map((center, i)=>{
+				
+				{centers(radius).map((center, i)=>{
 					return (
 						<Button key={center.label} sx={{
 							position: "absolute",
-							width: {md: 190, xs: 150},
-							height: {md: 190, xs: 150},
+							width: radius/1.47,
+							height: radius/1.47,
 							// transform: `translate(${x}px, ${y}px) rotate(${center.rotate || '0deg'})`,
 							...(center.position || {})
 						}}>
 							<Container sx={{
 								position: "relative",
-								width: {md: 170, xs: 125},
-								height: {md: 170, xs: 125},
+								width: radius/1.65,
+								height: radius/1.65,
 							}}>
 								<a href={center.endpoint}>
-									
 									<img src={`https://cfde-drc.s3.us-east-2.amazonaws.com/assets/img/${center.label}.png`} alt={center.label} loading="lazy" decoding="async"  sizes="100vw" style={{
 										position: "absolute",
 										height: "100%",
@@ -193,15 +196,15 @@ export default function InteractiveNavComponent() {
 										color: "transparent",
 										display: "block",
 										transform: `rotate(${center.rotate || '0deg'})`}}/>
-									<Typography sx={{color: "#FFF", position: "absolute", textTransform: "uppercase", fontSize: {md: 16, xs: 11.5}, ...(center.text_position || {})}}>
+									<Typography sx={{color: "#FFF", position: "absolute", textTransform: "uppercase", fontSize: radius/17.5, ...(center.text_position || {})}}>
 										<b>{center.label}</b>
 									</Typography>
-									<Container sx={{display: {md: 'block', xs: 'none'}}}>
-										<img src={`https://cfde-drc.s3.us-east-2.amazonaws.com/assets/img/${center.label} 1.png`} alt={center.label} style={{position: "absolute", width: 40, height: 40, zIndex: 100, ...(center.image_position || {})}}/>
+									<Container>
+										<img src={`https://cfde-drc.s3.us-east-2.amazonaws.com/assets/img/${center.label} 1.png`} alt={center.label} style={{position: "absolute", width: radius/7, height: radius/7, zIndex: 100, ...(center.image_position || {})}}/>
 									</Container>
-									<Container sx={{display: {md: 'none', xs: 'block'}}}>
+									{/* <Container sx={{display: {md: 'none', xs: 'block'}}}>
 										<img src={`https://cfde-drc.s3.us-east-2.amazonaws.com/assets/img/${center.label} 1.png`} alt={center.label} style={{position: "absolute", zIndex: 100, width: 30, height: 30, ...(center.image_position_small || {})}}/>
-									</Container>
+									</Container> */}
 									
 								</a>
 							</Container>
@@ -212,11 +215,10 @@ export default function InteractiveNavComponent() {
 					sx={{
 						background: "#fff", 
 						borderRadius: 1000, 
-						width: {md: 120, xs: 110}, 
-						height: {md: 120, xs: 110},
+						width: radius/2.3, 
+						height: radius/2.3,
 						position: "absolute",
-						top: {md: 395, xs: 365},
-						left: {md: 5, xs: -5},
+						transform: `translate(${radius/250}px, ${radius/1.14}px)`,
 						padding: 1,
 						zIndex: 100,
 					}}
@@ -225,9 +227,9 @@ export default function InteractiveNavComponent() {
 						<Box 
 							sx={{
 								position: 'relative',
-								width: {md: 100, xs: 80},
+								width: radius/3,
 								overflow: 'hidden',
-								height: {md: 100, xs: 80},
+								height: radius/3,
 								zIndex: 100,
 							}}
 						>
@@ -247,14 +249,15 @@ export default function InteractiveNavComponent() {
 					sx={{
 						background: "#000", 
 						borderRadius: "50%", 
-						width: {md: 230, xs: 200}, 
-						height: {md: 230, xs: 200},
+						width: radius/1.2, 
+						height: radius/1.2,
 						position: "absolute",
-						top: {md: 340, xs: 320},
-						left: {md: -50, xs: -50},
+						transform: `translate(${radius/250}px, ${radius/1.46}px)`,
 						opacity: "0.5"
 					}}
 				></Container>
-		</Container>
+			
+			</Container>
+		// </Container>
 	)
 }
