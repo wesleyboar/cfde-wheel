370b23a3bdeb0da8f2fe6c1d1947a12653f5d9ec
John Erol Evangelista <je.evangelista04@gmail.com>
2025-10-21 17:54:32 -0400

    added tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,14 +3,16 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: ['src/index.tsx'],
-    format: ['cjs'],
+    format: ['cjs', 'esm'],
     sourcemap: false,
     clean: true,
     outDir: 'dist',
+    minify: true,
+    dts: true
   },
   {
     entry: ['src/index.tsx'],
-    format: ['esm'],
+    format: ['esm', 'cjs'],
     platform: 'browser',
     outDir: 'dist',
     outExtension: () => ({ js: '.browser.js' }),
@@ -19,5 +21,6 @@ export default defineConfig([
     define: {
       'process.env.NODE_ENV': '"production"',
     },
+    minify: true
   },
 ]);
