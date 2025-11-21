e0fc7e8728979f1a1b1b7709381090102772169a
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-21 12:10:10 -0600

    refactor: abandon index.browser.js

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,18 +9,5 @@ export default defineConfig([
     outDir: 'dist',
     minify: true,
     dts: true
-  },
-  {
-    entry: ['src/index.tsx'],
-    format: ['esm', 'cjs'],
-    platform: 'browser',
-    outDir: 'dist',
-    outExtension: () => ({ js: '.browser.js' }),
-    external: ['react', 'react-dom'],
-    noExternal: [/@mui\/.*/, /@emotion\/.*/],
-    define: {
-      'process.env.NODE_ENV': '"production"',
-    },
-    minify: true
-  },
+  }
 ]);
